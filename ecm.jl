66c1f8fe836dc181a074cf7d26cbbf63ada08f0e
Cryptobscur <checkeurobscur@gmail.com>
2015-03-09 01:19:57 +0100

    Ajout support courbes Edwards
    Ajout des fonctions d'addition et de doublement pour les courbes
    d'Edwards, ecm_f ajustée

--- ecm.jl
+++ ecm.jl
@@ -1,6 +1,7 @@
 
-### ECM est pour l'instant configuré sur les courbes de Weierstrass, l'algo sera étendu
-### à d'autres courbes lorsque la fiabilité des tests avec Weierstrass sera établie.
+module ecm
+
+export ecm_f
 
 type point # Un point en coordonnees projectives #
 	X::BigInt
@@ -8,12 +9,12 @@ type point # Un point en coordonnees projectives #
 	Z::BigInt
 end
 
-point() = point(0, 0, 0)
+### WEIRSTRASS ###
 
 ## Addition de deux points non égaux P et Q sur une courbe de Weierstrass : y^2 = x^3 + ax + b ##
 function add_weierstrass(P::point, Q::point, a::BigInt, N::BigInt)
 	
-	R = point()
+	R = point(0, 1, 0)
 
 	## Si un des points est le point à l'infini (i.e l'élément neutre)
 	## alors la somme aura pour résultat l'autre point 
@@ -53,7 +54,7 @@ end
 ## Doublement d'un point P (= 2P) sur une courbe de Weierstrass : y^2 = x^3 + ax + b ##
 function double_weierstrass(P::point, a::BigInt, N::BigInt)
 	
-	R = point()
+	R = point(0, 1, 0)
 
 	# on effectue quelques calculs préliminaires pour accélérer le calcul global #
 
@@ -87,52 +88,142 @@ function double_weierstrass(P::point, a::BigInt, N::BigInt)
 
 end
 
+### EDWARDS ###
+
+## Addition de deux points non égaux P et Q sur une courbe d'Edwards : x^2 + y^2 = 1 + d * x^2 * y^2 ##
+function add_edwards(P::point, Q::point, d::BigInt, N::BigInt)
+
+	R = point(0, 1, 1)
+
+	## le calcul trouve sa source ici : http://www.hyperelliptic.org/EFD/g1p/auto-edwards-projective.html#addition-add-2007-bl ##
+
+	A::BigInt = (P.Z * Q.Z) % N
+	B::BigInt = (A^2) % N
+
+	C::BigInt = (P.X * Q.X) % N
+	D::BigInt = (P.Y * Q.Y) % N
+
+	E::BigInt = (d * C * D) % N
+
+	F::BigInt = (B - E) % N
+	G::BigInt = (B + E) % N
+
+	R.X = (A * F * ((P.X + P.Y) * (Q.X + Q.Y) - C - D)) % N
+	R.Y = (A * G * (D - C)) % N
+	R.Z = (F * G) % N # formule initiale : c * F * G
+
+	return R
+end
+
+## Doublement d'un point P (= 2P) sur une courbe d'Edwards : x^2 + y^2 = 1 + d * x^2 * y^2 ##
+function double_edwards(P::point, d::BigInt, N::BigInt)
+
+	## l'addition est unifiée sur les courbes d'Edwards, i.e pas de cas particulier pour deux points égaux
+	## mais du coup, on ne peut pas tester les points de torsion comme pour weierstrass (?)
+
+	return add_edwards(P, P, d, N);
+end
+
 ## Algorithme Double & Add pour calculer des expressions du type x * P, avec x un entier et P un point sur une courbe ##
-function double_and_add(P::point, x::BigInt, a::BigInt, N::BigInt)
-	
-	# utile ? #
-	if x < 0
-		x = -x
-		P.Y = -P.Y
-	end
+## 'vs' désigne la valeur dite "statique" dans la courbe elliptique employée (une constante en gros) ##
+function double_and_add(P::point, x::BigInt, vs::BigInt, N::BigInt, modele::Char) 
 
 	bin_x = digits(x, 2) # on récupère x en base deux
-	double_P = point()
+	
+	## julia n'a pas encore de switch (officiel du moins) pour l'instant... ##
 
-	for i = (length(bin_x) - 1):-1:1
-		
-		double_P = double_weierstrass(P, a, N) # double
+	if modele == 'w'
 
-		if bin_x[i] == 1
-			P = add_weierstrass(P, double_P, a, N) # add
+		double_P = point(0, 1, 0)
+
+		for i = (length(bin_x) - 1):-1:1
+			
+			double_P = double_weierstrass(P, vs, N) # double
+
+			if bin_x[i] == 1
+				P = add_weierstrass(P, double_P, vs, N) # add
+			end
 		end
 
+	elseif modele == 'e'
+
+		double_P = point(0, 1, 1)
+
+		for i = (length(bin_x) - 1):-1:1
+			
+			double_P = double_edwards(P, vs, N) # double
+
+			if bin_x[i] == 1
+				P = add_edwards(P, double_P, vs, N) # add
+			end
+		end
 	end
 
 	return P
-
 end
 
 ## à finaliser ##
-function ecm(x::BigInt, N::BigInt)
+function ecm_f(x::BigInt, N::BigInt, modele::Char)
 
-	## On initialise une courbe et un point sur cette courbe.
-	## On fixe la coordonnée Z à 1 afin de se retrouver sous la forme y^2 = x^3 + ax + b
-	## De cette façon, b est déterminé par le calcul de y^2 - x^3 - ax
-	## et on obtient un point valable, mais, au final, on ne s'en sert pas, donc à ignorer.
+	## julia n'a pas encore de switch (officiel du moins) pour l'instant... ##
 
-	# surement possible de faire qqc de plus "propre" #
-	a = BigInt(iround(BigFloat(rand() * (N - 1))))
+	if modele == 'w' # Weirstrass
 
-	## si jamais on fixe b à 1 (bien que cela ne change rien du point de vue du code)
-	## on peut alors prendre d'office le point (0:1:1) et on s'évite les deux lignes de code ci-dessous
+		## On initialise une courbe et un point sur cette courbe.
+		## On fixe la coordonnée Z à 1 afin de se retrouver sous la forme y^2 = x^3 + ax + b
+		## De cette façon, b est déterminé par le calcul de y^2 - x^3 - ax
+		## et on obtient un point valable (au final, on ne se sert pas de b, donc à ignorer)
 
-	# X = BigInt(iround(BigFloat(rand() * (N - 1)))) 
-	# Y = BigInt(iround(BigFloat(rand() * (N - 1))))
+		# surement possible de faire qqc de plus "propre" #
+		a::BigInt = iround(BigFloat(rand() * (N - 1)))
 
-	P = point(0, 1, 1) # mais est-ce un choix judicieux ?
+		## si jamais on fixe b à 1 (bien que cela ne change rien du point de vue du code) on peut alors
+		## prendre d'office le point (0:1:1) et on s'évite les deux lignes de code ci-dessous, mais est-ce un choix judicieux ?
 
-	R::point = double_and_add(P, x, a, N)
+		Xw::BigInt = iround(BigFloat(rand() * (N - 1)))
+		Yw::BigInt = iround(BigFloat(rand() * (N - 1)))
 
-	return BigInt(gcd(R.Z, N))
+		Pw = point(Xw, Yw, 1)
+
+		Rw::point = double_and_add(Pw, x, a, N, 'w')
+
+		return BigInt(gcd(Rw.Z, N))
+
+	elseif modele == 'e' # Edwards
+
+		## On initialise une courbe et un point sur cette courbe.
+		## On fixe la coordonnée Z à 1 afin de se retrouver sous la forme x^2 + y^2 = 1 + d * x^2 * y^ 2
+		## De cette façon, d est déterminé par le calcul de (x^2 + y^2 - 1) / (x^2 * y^2)
+		## et on obtient un point valable (l'inversion est ici inévitable ?)
+
+		Xe::BigInt = iround(BigFloat(rand() * (N - 1)))
+		Ye::BigInt = iround(BigFloat(rand() * (N - 1)))
+
+		Xe2 = (Xe^2) % N
+		Ye2 = (Ye^2) % N
+
+		tmp = Xe2 * Ye2 # = x^2 * y^2
+
+		## Julia est plutot "chiant" au niveau de gmp car il transforme les valeurs de retour de certaines fonctions en erreurs/exceptions
+		## Il faut donc aller voir le code julia associé pour déterminer le type d'erreur et se coltiner sur le dos des try/catch...
+
+		try tmp = invmod(tmp, N) # on tente d'inverser x^2 * y^2
+		catch test
+			if isa(test, ErrorException) # si l'inversion n'est pas possible, on a trouvé un facteur de N
+				println("inversion impossible !")
+				return BigInt(gcd(tmp, N))
+			end
+		end
+
+		d::BigInt = ((Xe2 + Ye2 - 1) * tmp) % N # d = (x^2 + y^2 - 1) / (x^2 * y^2)
+
+		Pe = point(Xe, Ye, 1)
+
+		Re::point = double_and_add(Pe, x, d, N, 'e')
+
+		zero::BigInt = BigInt(gcd(Re.X, N))
+		return zero
+	end
+end
+
 end
